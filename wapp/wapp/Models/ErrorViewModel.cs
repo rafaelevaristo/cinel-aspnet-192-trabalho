9bc1a8f79cb8f200f9cc0508bed41f4f4fefcc00
rafael.evaristo <rafael.evaristo@gmail.com>
2025-01-07 20:56:38 +0000

    Add: Initial commit from Visual studio template.

---
+++ wapp/wapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
